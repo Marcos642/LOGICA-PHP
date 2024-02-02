5c2a79535c4a5a5e6e0c42482ac3ac2d7e309e71
Marcos Tadeu de Brito Torres <94060220+Marcos642@users.noreply.github.com>
2024-02-02 16:19:14 -0300

    Update array_sub_placeholders.php

--- Logica/array_sub_placeholders.php
+++ Logica/array_sub_placeholders.php
@@ -1,30 +1,30 @@
 <?php
 # html dentro de variavel
 $htmlTEMPLATE = <<<HTML
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Nome: {{name}}</h1>
-    <p>Descrição: {{description}}</p>
-</body>
-</html>
-HTML;
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Nome: {{name}}</h1>
+        <p>Descrição: {{description}}</p>
+    </body>
+    </html>
+    HTML;
 // DADOS PARA SUBSTITUIR AS PLACEHOLDERS
-$data = [
-    "name"=>"John Doe",
-    "description"=>"Lorem ipsum dolor sit amet."
-];
+    $data = [
+        "name"=>"John Doe",
+        "description"=>"Lorem ipsum dolor sit amet."
+    ];
 # Pegar as chaves
-$keys = array_keys($data);
+    $keys = array_keys($data);
 # Mapear e modificar
-$map = array_map(function($sub){
-    return "{{".$sub."}}";
-},$keys);
+    $map = array_map(function($sub){
+        return "{{".$sub."}}";
+    },$keys);
 #Unir tudo
-$replace = str_replace($map,array_values($data),$htmlTEMPLATE);
-echo $replace;
+    $replace = str_replace($map,array_values($data),$htmlTEMPLATE);
+    echo $replace;
