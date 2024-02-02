fe4db83822170108bc680387b75d1f3212739a61
Marcos Tadeu de Brito Torres <94060220+Marcos642@users.noreply.github.com>
2024-02-02 10:30:29 -0300

    Update array.php

--- Logica/array.php
+++ Logica/array.php
@@ -76,8 +76,3 @@
             # SUBISTITUIÇÃO COM STR_REPLACE
             # 1 - CHAVES KEY, 2 - ARRAY_VALUES(ARRAY ASSOCIATIVO), - ONDE SERA APLICADO
             $replace = str_replace($keys,array_values($assoc),$string);
-<<<<<<< Updated upstream
-            echo $replace;
-=======
-            echo $replace;
->>>>>>> Stashed changes
