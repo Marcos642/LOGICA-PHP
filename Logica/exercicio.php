e1ef34d4b15ba4550f06d0bd7b8896e94aa253a3
Marcos Tadeu <marcostadeu642@gmail.com>
2024-02-08 00:02:32 -0300

    Exercicios

---
+++ Logica/exercicio.php
@@ -0,0 +1,66 @@
+ <!-- 1) - Crie um algoritmo que receba um número digitado pelo usuário e verifique se esse valor é positivo, negativo ou igual a zero. A saída deve ser: "Valor Positivo", "Valor Negativo" ou "Igual a Zero". -->
+
+<!-- <body>
+    <form method="POST">
+        <input type="text" placeholder="Digite um Numero" name="valor">
+        <input type="submit" value="Enviar">
+    </form>
+</body> -->
+<?php
+// if(isset($_POST['valor'])){
+//     $valor = $_POST['valor'];
+//     if($valor > 0){
+//         echo "O valor ".$valor." é positivo";
+//     }else if($valor == 0){
+//         echo "O valor ".$valor." é igual a zero";
+//     }else{
+//         echo "O valor ".$valor." é negativo";
+//     }
+// }
+
+// 2) Crie um algoritmo que solicite a entrada de um número, e exiba a        tabuada de 0 a 10 de acordo com o número solicitado, ex: 
+// Entrada = 4
+// Saída = 4 X 0 = 0...4 X 10 = 40.
+?>
+<!-- <body>
+    <form method="POST">
+        <input type="text" placeholder="Digite um numero" name="num">
+        <input type="submit">
+    </form>
+</body> -->
+<?php
+// if(isset($_POST['num'])){
+//     $num = $_POST['num'];
+//     for($i = 0;$i <= 10; $i++){
+//         echo "<br>";
+//         echo $i * $num;
+//         echo "<br>";
+//     }
+// }
+?>
+
+<!-- 3) Crie um algoritmo que solicite um número, e faça o cálculo fatorial do mesmo, exiba o resultado na tela. Ex: Entrada = 3 Processamento: (3 * 2) * 1 Saída: 6 -->
+
+<!-- <body>
+    <form method="POST">
+        <input type="text" name="val" placeholder="Valor">
+        <input type="submit" name="enviar">
+    </form>
+</body> -->
+
+<?php
+// if(isset($_POST['val'])){
+//     $num = $_POST['val'];
+//     if($num > 0){
+//         $valor = $num;
+//         for($i = ($valor - 1); $i > 0; $i--){
+//           $valor = $valor * $i;
+//         }
+//     }else{
+//         $valor = 0;
+//     }
+//     echo "!{$num} = {$valor}";
+// }
+?>
+
+<!-- 4) Crie um programa em que o usuário escolha uma operação (soma, subtração, multiplicação ou divisão). Crie duas caixas de texto para receber 2 números. Realize a operação escolhida em cada um dos números.  -->
