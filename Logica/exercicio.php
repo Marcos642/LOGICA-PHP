e280b7e2856e590eab2ecc8bfe3fcac7709e02e8
Marcos Tadeu de Brito Torres <94060220+Marcos642@users.noreply.github.com>
2024-02-08 00:08:29 -0300

    Update exercicio.php

--- Logica/exercicio.php
+++ Logica/exercicio.php
@@ -1,4 +1,4 @@
-LINK DOS EXERCÍCIOS: https://satellasoft.com/artigo/php/lista-de-exercicios-em-php
+<!-- LINK DOS EXERCÍCIOS: https://satellasoft.com/artigo/php/lista-de-exercicios-em-php -->
 
 <!-- 1) - Crie um algoritmo que receba um número digitado pelo usuário e verifique se esse valor é positivo, negativo ou igual a zero. A saída deve ser: "Valor Positivo", "Valor Negativo" ou "Igual a Zero". -->
 
