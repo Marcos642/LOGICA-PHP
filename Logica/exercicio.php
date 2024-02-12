79d66f8eb959f862d1836e43c552758a0799953b
Marcos Tadeu <marcostadeu642@gmail.com>
2024-02-12 15:30:28 -0300

    Update exercicio.php

--- Logica/exercicio.php
+++ Logica/exercicio.php
@@ -67,4 +67,63 @@
 
 <!-- 4) Crie um programa em que o usuário escolha uma operação (soma, subtração, multiplicação ou divisão).
 Crie duas caixas de texto para receber 2 números. Realize a operação escolhida em cada um dos números.  -->
-<p>Teste</p>
+<?php
+// $num1 = filter_input(INPUT_POST, 'numero1');
+// $num2 = filter_input(INPUT_POST, 'numero2');
+// $operacao = filter_input(INPUT_POST, 'operacao');
+// $result = "";
+
+// if($num1 && $num2){
+//     switch ($operacao) {
+//         case '+':
+//             $operacao = $num1 + $num2;
+//         break;
+//         case '-':
+//             $operacao = $num1 - $num2;
+//         break;
+//         case '*':
+//             $operacao = $num1 * $num2;
+//         break;
+//         case '/':
+//             $operacao = $num1 / $num2;
+//         break;
+//     }
+// }
+?>
+<!-- <body>
+    <?= $operacao ?>
+    <form method="POST">
+        <label>Numero 1:<input type="text" name="numero1"></label>
+        <label>Numero 2:<input type="text" name="numero2"></label>
+        <label>operação
+            <select name="operacao">
+                <option value="+">Adição</option>
+                <option value="-">Subtração</option>
+                <option value="*">multiplicação</option>
+                <option value="/">Divisão</option>
+            </select>
+        </label>
+        <input type="submit" name="btnCalcular" value="math">
+    </form>
+</body> -->
+
+<!-- 5) Solicite a entrada de um número e descubra se um número digitado é par ou ímpar.  -->
+<?php
+// $num = 3;
+// if($num % 2 == 0){
+//     echo "Par";
+// }else{
+//     echo "Imp";
+// }
+?>
+<!-- 6) Faça um algoritmo PHP que receba os valores A e B, imprima-os em ordem crescente em relação aos seus valores. Exemplo, para A=5, B=4. Você deve imprimir na tela: "4 5". -->
+<?php
+$A = 5; 
+$B = 7;
+if($A > $B){
+    echo "{$A} {$B}";
+}else{
+    echo "{$B} {$A}";
+}
+
+// 7) Faça um algoritmo em PHP onde verifica se o valor da variável A é maior ou menor que o valor da variável B. A mensagem a ser impressa deve ser “A maior que B” ou “A menor que B”.
